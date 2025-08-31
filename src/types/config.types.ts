479f4530253ded0317a86d63a3eab4c68eb0216d
Muhammad Sulthan K M <119175215+MuhdSulthan@users.noreply.github.com>
2025-08-31 19:08:12 +0530

    Update config.types.ts

--- src/types/config.types.ts
+++ src/types/config.types.ts
@@ -142,12 +142,9 @@ export type SchemaConfig = {
  * Social links for organization.
  */
 export type SameAsConfig = {
-  threads: string;
   linkedin: string;
-  discord: string;
   tryhackme: string;  
   credly: string;  
-  instagram: string;
 };
 
 /**
